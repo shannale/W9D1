1a1964c5915d4943c78ba3e2059ece3700d2b3c5
Shanna Le <shannatle@gmail.com>
2023-05-22 15:08:41 -0700

    Finish movingObject class

--- Asteroids/src/index.js
+++ Asteroids/src/index.js
@@ -5,4 +5,23 @@ const movingObject = require("./moving_object.js")
 window.MovingObject = MovingObject;
 
 // console.log("Webpack is working!")
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+  });
+
+window.mo = mo;
+
+
+
+const canvasEl = document.getElementById("game-canvas")
+
+const ctx = canvasEl.getContext("2d")
+
+window.ctx = ctx;
+
+
+
 
--- Asteroids/src/moving_object.js
+++ Asteroids/src/moving_object.js
@@ -1,22 +1,51 @@
-
-
 class MovingObject {
     // const canvasElement = 
-    constructor() {
-    this.pos = pos 
-    this.vel = vel
-    this.radius = radius
-    this.color = color
-    }
-}
-const movingObject = () => {
-    const mo = new MovingObject({
-        pos: [30, 30],
-        vel: [10, 10],
-        radius: 5,
-        color: "#00FF00"
-      });
-}
+    constructor(options) {
+    this.pos = options.pos 
+    this.vel = options.vel
+    this.radius = options.radius
+    this.color = options.color
+    };
 
+    // draw(ctx) {
+    //   ctx.beginPath();
+    //   ctx.arc(200, 75, 50, 0, 2 * Math.PI, true);
+    //   ctx.stroke();
+
+    // };
+
+    draw(ctx) {
+      ctx.beginPath();
+      ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, true);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+  };
+
+  move(pos, vel) {
+    let newPos = [];
+      pos[0] += vel[0];
+      pos[1] += vel[1];
+  
+      newPos = [pos[0],pos[1]];
+      return newPos
+  }
+
+};
+
+
+// const movingObject = () => {
+//   const mo = new MovingObject({
+//       pos: [30, 30],
+//       vel: [10, 10],
+//       radius: 5,
+//       color: "#00FF00"
+//     });
+// }
 
   export default MovingObject;
+
+
+// checking draw(ctx) 
+// check move 
+// check moving object instance
+// check move
