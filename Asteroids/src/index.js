25111005b8a6e31d61a088ef71591e475aa335de
a-wong-8 <awong88@berkeley.edu>
2023-05-22 14:09:07 -0700

    .

--- Asteroids/src/index.js
+++ Asteroids/src/index.js
@@ -1,1 +1,8 @@
-console.log("Webpack is working!")
+import MovingObject from "./moving_object.js";
+
+const movingObject = require("./moving_object.js")
+
+window.MovingObject = MovingObject;
+
+// console.log("Webpack is working!")
+
---
+++ Asteroids/src/moving_object.js
@@ -0,0 +1,22 @@
+
+
+class MovingObject {
+    // const canvasElement = 
+    constructor() {
+    this.pos = pos 
+    this.vel = vel
+    this.radius = radius
+    this.color = color
+    }
+}
+const movingObject = () => {
+    const mo = new MovingObject({
+        pos: [30, 30],
+        vel: [10, 10],
+        radius: 5,
+        color: "#00FF00"
+      });
+}
+
+
+  export default MovingObject;
